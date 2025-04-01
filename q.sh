964ed005e42368a81f718247f20366aa56e06cf2
wgc-research <41206703@qq.com>
2025-04-01 11:20:13 +0800

    update

--- q.sh
+++ q.sh
@@ -1,4 +1,7 @@
-<!DOCTYPE html>
+#!/bin/bash
+
+# Define the redirect HTML content
+REDIRECT_HTML='<!DOCTYPE html>
 <html lang="en-US">
 <head>
   <meta charset="UTF-8">
@@ -12,4 +15,16 @@
   <p>Redirecting to FedKDD 2025 workshop page...</p>
   <p>If you are not redirected automatically, please click <a href="https://fedkdd.github.io/fedkdd2025/">here</a>.</p>
 </body>
-</html>
+</html>'
+
+# Find all index.html files except the one in the root directory
+for file in $(find . -path "./*/index.html" -type f); do
+  echo "Updating $file"
+  echo "$REDIRECT_HTML" > "$file"
+done
+
+# Update 404.html
+echo "Updating 404.html"
+echo "$REDIRECT_HTML" > "404.html"
+
+echo "All files updated successfully!"
